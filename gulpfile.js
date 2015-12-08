e3f04691d42877509e423fff9af44430473b5cf2
conormcafee <hello@conormcafee.co.uk>
2015-12-08 19:41:20 +0000

    UI Challenge
    UI Challenge Start

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,18 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('production/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
+
+// SASS (UI)
+gulp.task('sass-ui', function() {
+    return sass('build/sass/ui.scss', { style: 'expanded' })
+    .pipe(prefix({ browsers: ['last 2 versions'] }))
+    .pipe(gulp.dest('production/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('production/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
 // Scripts
 gulp.task('scripts', function() {
   return gulp.src('build/js/*.js')
