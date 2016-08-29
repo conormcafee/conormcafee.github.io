660e1fd88e79ff5d7adfb22026f495b8cdfa9d61
conor-mcafee <hello@conormcafee.co.uk>
2016-08-29 17:36:27 +0100

    First step in redesign

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 /*!
  * gulp
- * $ npm install gulp gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename gulp-cache del --save-dev
+ * npm install gulp gulp-ruby-sass gulp-autoprefixer gulp-livereload gulp-cssnano jshint gulp-jshint gulp-concat gulp-uglify gulp-notify gulp-rename gulp-cache del gulp-svgmin gulp-inline-source gulp-imagemin --save-dev
  */
- 
+
 // Load plugins
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
-    minifycss = require('gulp-minify-css'),
+    cssnano = require('gulp-cssnano'),
     prefix = require('gulp-autoprefixer'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
@@ -14,67 +14,73 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
-    minifyHTML = require('gulp-minify-html'),
-    del = require('del');
- 
+    del = require('del'),
+    svgmin = require('gulp-svgmin'),
+    imagemin = require('gulp-imagemin'),
+    inlinesource = require('gulp-inline-source'),
+    livereload = require('gulp-livereload');
+
+// inlinesource
+
+gulp.task('is', function () {
+  return gulp.src('craft/templates/_critical/_critical-css.html')
+    .pipe(inlinesource())
+    .pipe(gulp.dest('craft/templates/_includes'));
+});
+
 // SASS
 gulp.task('sass', function() {
-    return sass('build/sass/main.scss', { style: 'expanded' })
-    .pipe(prefix({ browsers: ['last 2 versions'] }))
-    .pipe(gulp.dest('production/css'))
+    return sass('build/sass/style.scss', { style: 'expanded' })
+    .pipe(prefix({ browsers: ['ie 9', 'last 4 versions'] }))
+    .pipe(gulp.dest('assets/css'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('production/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(cssnano())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }))
+    .pipe(livereload());
 });
 
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('build/js/*.js')
+  return gulp.src(['build/js/vendor/modernizr.custom.js', 'build/js/vendor/jquery-2.2.3.js', 'build/js/vendor/echo.js', 'build/js/vendor/unslider.js', 'build/js/scripts.js'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('production/js'))
+    .pipe(gulp.dest('assets/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('production/js'))
+    .pipe(gulp.dest('assets/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-// Minify HTML
-gulp.task('minify-html', function() {
-  var opts = {
-    conditionals: true,
-    spare:true
-  };
- 
-  return gulp.src('./_site/*.html')
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./_site'))
-    gulp.src("./_site/*/*.html")
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest("./_site/./"));
+// SVGOMG
+gulp.task('svg', function () {
+  return gulp.src('build/icons/*.svg')
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+  .pipe(gulp.dest('assets/images/icons'));
 });
- 
-// Clean
-gulp.task('clean', function(cb) {
-    del(['dist/assets/css', 'dist/assets/js'], cb)
-});
- 
+
+gulp.task('images', () =>
+	gulp.src('build/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('assets/images'))
+);
+
+
 // Default task
 gulp.task('default', ['clean'], function() {
     gulp.start('sass', 'scripts');
 });
- 
+
 // Watch
 gulp.task('watch', function() {
- 
-  // Watch .scss files
-  gulp.watch('build/sass/**/*.scss', ['sass']);
- 
-  // Watch .js files
-  gulp.watch('build/js/*.js', ['scripts']);
-
-  gulp.watch('build/js/*.js', ['scripts']);
- 
+    livereload.listen();
+    gulp.watch('build/sass/**/*.scss', ['sass']);
+    gulp.watch('build/images/*', ['images']);
+    gulp.watch('build/icons/*', ['svg']);
+    gulp.watch('build/js/*.js', ['scripts']);
 });
